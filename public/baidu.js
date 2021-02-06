86866fb2041146a48d64d5e398be7515c1b81846
zackdk <zackvvzackvv@gmail.com>
2021-02-06 16:44:36 +0800

    添加百度统计

---
+++ public/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?33e22361d02a3a5ccfa83e092b027166';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
