6f10db47cacbdaf40709ca0cf7f52d28851f27a6
zackdk <zackvvzackvv@gmail.com>
2021-02-26 17:36:05 +0800

    更改百度统计

--- public/baidu.js
+++ public/baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?33e22361d02a3a5ccfa83e092b027166';
-  var s = document.getElementsByTagName('script')[0];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fa87ddc53c2456da86ad9beaa3f0e2a1";
+  var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
