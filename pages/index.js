8bdda01c29fd74320ecd7f1269731115e621588f
dongkai <958059970@qq.com>
2021-11-16 20:42:40 +0800

    ts学习部分笔记

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
     <Container>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          行者、空山的博客
+          行者、空山的网站
         </h1>
         <h2 className="text-gray-600 dark:text-gray-400 mb-16">
-          一个怀揣梦想的前端工程师，喜欢发呆，喜欢看书，喜欢思考，喜欢旅行。
+          怎么找，也找不到我要的快乐，怎么笑，也笑不出颜色...
         </h2>
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
           推荐文章
