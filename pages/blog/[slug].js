2ff87c12eb0dcc02069455de5e35f423fd9be149
zackdk <zackvvzackvv@gmail.com>
2021-12-19 22:08:38 +0800

    fix

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,7 +2,6 @@ import { MDXRemote } from 'next-mdx-remote';
 
 import { getFiles, getFileBySlug } from '@/lib/mdx';
 import BlogLayout from '@/layouts/blog';
-import MDXComponents from '@/components/MDXComponents';
 
 export default function Blog({ mdxSource, frontMatter }) {
   return (
