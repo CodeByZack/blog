b4feb9d7cf5b9e4d24e8010295b929e1a08ffdd8
dongkai <958059970@qq.com>
2022-08-02 14:19:14 +0800

    pwa支持

--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -14,6 +14,30 @@ import { BlogComponents } from 'ui';
 import { SessionProvider } from 'next-auth/react';
 import { CssBaseline, GeistProvider } from '@geist-ui/core';
 
+const PWA_HEAD_INFO = (
+  <>
+    <meta name="application-name" content="行者、空山的博客" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-title" content="行者、空山的博客" />
+    <meta name="description" content="行者、空山的博客，记录一些碎碎生活" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="theme-color" content="#000000" />
+
+    <link rel="apple-touch-icon" href="/avatar.jpg" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/avatar.jpg" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/avatar.jpg" />
+    <link rel="apple-touch-icon" sizes="167x167" href="/avatar.jpg" />
+
+    <link rel="icon" type="image/png" sizes="32x32" href="/avatar.jpg" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/avatar.jpg" />
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="mask-icon" href="/avatar.jpg" color="#5bbad5" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+  </>
+);
+
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <GeistProvider themeType="dark">
@@ -27,6 +51,7 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
                   content="width=device-width, initial-scale=1"
                   name="viewport"
                 />
+                {PWA_HEAD_INFO}
               </Head>
               <DefaultSeo {...SEO} />
               <Component {...pageProps} />
