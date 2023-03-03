1f3e81d0282fed1467a481b29e82967373785944
董凯 <958059970@qq.com>
2023-03-03 06:09:58 +0000

    添加评论区的主题切换

--- src/static/theme.js
+++ src/static/theme.js
@@ -1,19 +1,35 @@
 const theme = localStorage.getItem("theme");
 document.querySelector('html').className = theme;
 
-const changeTheme = ()=>{
+const changeTheme = () => {
     const html = document.querySelector('html');
     const themeNow = html.className;
-    if(themeNow === "dark"){
+    if (themeNow === "dark") {
         html.className = "light";
-        localStorage.setItem("theme","light");
-    }else{
+        changeGiscusTheme('light');
+        localStorage.setItem("theme", "light");
+    } else {
         html.className = "dark";
-        localStorage.setItem("theme","dark");
+        changeGiscusTheme('dark');
+        localStorage.setItem("theme", "dark");
     }
 };
 
-document.addEventListener('DOMContentLoaded',function(){
+const changeGiscusTheme = (theme) => {
+    // const theme = document.documentElement.getAttribute('data-theme') === 'dark' ?  'dark' : 'light'
+    const sendMessage = (message) => {
+        const iframe = document.querySelector('iframe.giscus-frame');
+        if (!iframe) return;
+        iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+    }
+    sendMessage({
+        setConfig: {
+            theme: theme
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     const themeBtn = document.querySelector(".page-header_button");
-    themeBtn.addEventListener("click",changeTheme);
+    themeBtn.addEventListener("click", changeTheme);
 });
